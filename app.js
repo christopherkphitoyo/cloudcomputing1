44508191bc602a2896f0e2ae78507b82b32f98d9
christopherkphitoyo <93181046+christopherkphitoyo@users.noreply.github.com>
2024-04-02 11:43:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const HOST = '0.0.0.0'
 const app = express()
 app.get('/', (req, res) => {
     res.send('christopher kp\n')
+    res.send('nim: 52022901\n')
+    res.send('jurusan : teknik informatika\n')
+   
 })
 
 app.listen(PORT, HOST)
